c5ea152a4c2e5ab4f79d0173f79e9a95d6f99d67
= <=>
2019-12-29 23:00:33 -0300

    Export withFormera.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Form from './Form';
 import Field from './Field';
 import FieldArray from './FieldArray';
+import withFormera from './withFormera';
 
 export * from './types';
 
-export { Form, Field, FieldArray }
+export { Form, Field, FieldArray, withFormera }
