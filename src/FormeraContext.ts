148486d85a8ab838483d5f5ff388b7b49ec6a2c4
= <=>
2019-12-30 01:17:31 -0300

    Calculando bits no context.

--- src/FormeraContext.ts
+++ src/FormeraContext.ts
@@ -1,4 +1,11 @@
 import React from 'react';
 import { IFormeraContext } from "./types";
 
-export const FormeraContext = React.createContext<IFormeraContext>({ formera: null });
+function calculateBytes(prev: IFormeraContext, next: IFormeraContext){
+  if(prev.formera === next.formera){
+    return 1;
+  }
+  return 0;
+}
+
+export const FormeraContext = React.createContext<IFormeraContext>({ formera: null }, calculateBytes);
