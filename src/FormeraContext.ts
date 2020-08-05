6e2326e80253798a569066196c6b601d33d06434
Felipe Laera Ignacio <laera.felipe@gmail.com>
2020-08-05 16:57:27 -0300

    Fixes.

--- src/FormeraContext.ts
+++ src/FormeraContext.ts
@@ -1,11 +1,11 @@
 import React from 'react';
 import { IFormeraContext } from "./types";
 
-function calculateBytes(prev: IFormeraContext, next: IFormeraContext){
-  if(prev.formera !== next.formera){
+function calculateBytes(prev: IFormeraContext, next: IFormeraContext) {
+  if (prev.formera !== next.formera) {
     return 1;
   }
   return 0;
 }
 
-export const FormeraContext = React.createContext<IFormeraContext>({ formera: null }, calculateBytes);
+export const FormeraContext = React.createContext<IFormeraContext>({ formera: null, unregisterFieldOnUnmount: false }, calculateBytes);
